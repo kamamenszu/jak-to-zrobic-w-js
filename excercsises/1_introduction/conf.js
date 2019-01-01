e2a46eb5a3e9f30057e2be5c93d8c81125ffa9da
Kamila Menes-Szuster <kamila.menes@gmail.com>
2019-01-01 23:05:53 +0100

    poprawka_zadania_1

--- excercsises/1_introduction/conf.js
+++ excercsises/1_introduction/conf.js
@@ -15,12 +15,12 @@ exports.config = {
 	  browser.manage().window().setSize(1680, 1050);	
 	  browser.ignoreSynchronization = true; 	
 
-      
+      /* 
 	  var SpecReporter = require('jasmine-spec-reporter');
 	  jasmine.getEnv().addReporter(new SpecReporter({
         displayStacktrace: true
       })); 
-	  
+	   */
 	  
       /* 
 	  var jasmineReporters = require('jasmine-reporters');
@@ -31,10 +31,10 @@ exports.config = {
       }));
 	  */
 
-      
+       /* 
 	  var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 	  jasmine.getEnv().addReporter( new Jasmine2HtmlReporter({}));
-	  
+	    */
       
     },
 
