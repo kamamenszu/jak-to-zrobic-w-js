f10e9a097a0719cd7d8e7bb2eab7b4bec381e345
kamamenszu <kamila.menes@gmail.com>
2019-01-02 15:16:57 +0100

    zaczete_zadanie

--- excercsises/3_synchronization/pages/contact.js
+++ excercsises/3_synchronization/pages/contact.js
@@ -1,6 +1,12 @@
 var Contact = function() {
 
   this.menuItems = element.all(by.css('ul.nav > li > a')); 
+  this.leftField1 = element(by.css('#name'));
+  this.leftField2 = element(by.css('#email'))
+  this.leftField3 = element(by.css('#content'));
+  this.leftButton = element(by.css('fieldset > button'));
+  this.header = element(by.xpath('//span[1]/h3[1]'));
+
 
   this.get = function() {
     browser.driver.get('http://jacekokrojek.github.io/jak-to-zrobic-w-js/contact.html');
@@ -14,6 +20,25 @@ var Contact = function() {
     this.menuItems.get(idx).click();
   };
 
+  this.sendKeysLeftField1 = function() {
+    this.leftField1.sendKeys('Kamila')
+  }
+
+  this.sendKeysLeftField2 = function() {
+    this.leftField2.sendKeys('malpa@wp.pl')
+  }
+
+  this.sendKeysLeftField3 = function() {
+    this.leftField3.sendKeys('Dzień dobry')
+  }
+
+  this.clickLeftButton = function() {
+    this.leftButton.click();
+  }
+
+  this.getHeader = function() {
+    return this.header.getText();
+  }
 };
 
 module.exports = new Contact();
