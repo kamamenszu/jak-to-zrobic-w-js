508ec36a629efc6c9f799fe35a5139623cf33503
kamamenszu <kamila.menes@gmail.com>
2019-01-02 13:11:29 +0100

    poprawki_2_zadania

--- excercsises/2_page_object/contact.spec.js
+++ excercsises/2_page_object/contact.spec.js
@@ -1,4 +1,3 @@
-var homePage = require('./pages/home');
 var contact = require('./pages/contact');
 
 describe('Protractor Workshop app', function() {
@@ -8,17 +7,7 @@ describe('Protractor Workshop app', function() {
 	});
 
 	it('should have "Get in touch" and "Address" sections on Contact Us page', function(){
-		var headerGet = element(by.xpath('//fieldset/h3'));
-		var expectedHeaderGet = "Get in Touch";
-		expect(headerGet.getText()).toContain(expectedHeaderGet);
-		expect(element(by.css('input[placeholder="Name"]')).isDisplayed()).toBe(true);
-		expect(element(by.css('input[placeholder="Email"]')).isDisplayed()).toBe(true);
-		expect(element(by.css('textarea[placeholder="Your Message"]')).isDisplayed()).toBe(true);
-		expect(element(by.xpath('//fieldset/button')).isDisplayed()).toBe(true);
-		var headerAddress = element(by.xpath('//span[2]/h3'));
-		var expectedHeaderAddress = "Address";
-		expect(headerAddress.getText()).toBe(expectedHeaderAddress);
-		var addresses = element.all(by.css('.span6 address'));
-		expect(addresses.count()).toEqual(2);
+		expect(contact.getLeftHeader()).toEqual("Get in Touch");
+		expect(contact.getRightHeader()).toEqual("Address");
 	});
 });
